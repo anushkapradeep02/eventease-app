d5ecee90ee97082df8be5a0860a810fcb46a4579
anushkapradeep02 <169102658+anushkapradeep02@users.noreply.github.com>
2025-04-02 20:10:42 -0700

    Initial commit: Complete EventEase Blazor App

---
+++ EventEaseApp/Data/Event.cs
@@ -0,0 +1,10 @@
+namespace EventEaseApp.Data
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
